37e8d5df9d9b1a02893e38e700ea726afe07f50d
Tereza Z <zajacova.tereza@gmail.com>
2022-11-25 20:31:46 +0100

    ip filter

--- sample_data_parsing.py
+++ sample_data_parsing.py
@@ -1,6 +1,7 @@
 import json 
 from datetime import datetime
 import pytz
+import re
 
 # Parsing přiložených dat: seznam lxc kontejnerů na testovacím serveru ve formátu JSON, výstupem pro každý kontejner: name, cpu a memory usage, created_at, status a všechny přiřazené IP adresy.
 
@@ -15,7 +16,6 @@ with open('./sample-data.json') as json_file:
         item['addresses'] = []
         item['cpu'] = None
         item['memory_usage'] = None
-        item['created_at'] = i['created_at']
         
 #convert datetime to UTC timestamp      
         cre_at_date = datetime.strptime(i['created_at'], "%Y-%m-%dT%H:%M:%S%z")
@@ -33,12 +33,12 @@ with open('./sample-data.json') as json_file:
                     address_list = i['state']['network'][address_key][addd_key]
 
                     for x in address_list:
-                        item['addresses'].append(x['address'])
-                        #print(x['address'])
+                        if re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", x['address']):
+                            item['addresses'].append(x['address'])
+                            
             item['memory_usage'] = i['state']['memory']['usage'] 
             item['cpu'] = i['state']['cpu']['usage']
         response.append(item)
-
 
 
 print(response)
